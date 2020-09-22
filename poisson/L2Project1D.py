6c91fabee3af29c6b26f74f72bea6be2c699cbbe
maqiang809 <maqiang@lsec.cc.ac.cn>
2020-09-22 12:18:18 +0800

    modified

---
+++ poisson/L2Project1D.py
@@ -0,0 +1,9 @@
+import numpy as np
+from fes.FES1DL21 import FES1DL21
+from mesh.MeshGenerator import line1DL2
+from numerics.SparseMatrix import SMatrix
+from scipy.sparse.linalg import *
+mesh = line1DL2(0.0, 1.0, 10)
+mesh.printNodes()
+mesh.printElements()
+mesh.printBoundaries()
